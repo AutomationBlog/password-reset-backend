7dbaee4a9f3cbc9f7873d107bcbc60c99a878b96
automationblog <sawin24@gmail.com>
2024-06-30 10:44:51 +0530

    server created

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const Dotenv = require("dotenv");
+const app = express();
+app.use(cors());
+Dotenv.config();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
