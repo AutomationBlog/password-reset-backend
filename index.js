5ca82b289b095df7d42003aacbe260e649886781
automationblog <sawin24@gmail.com>
2024-07-06 19:33:29 +0530

    db created

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const express = require("express");
-const cors = require("cors");
-const Dotenv = require("dotenv");
+import express from "express";
+import cors from "cors";
+import Dotenv from "dotenv";
+
+import connectToDb from "./dbUtils/mongodb-connection.js";
+import usersRouter from "./routes/users_db.js";
 const app = express();
 app.use(cors());
 Dotenv.config();
@@ -9,6 +12,10 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+
+await connectToDb();
+
+app.use("/users", usersRouter); // Users Router
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
