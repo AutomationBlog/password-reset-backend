865d04035c24d5e87875d307bec233f277f1969f
automationblog <sawin24@gmail.com>
2024-10-12 14:14:03 +0530

    gen. cookies updated

--- utils/generateTokenAndSetCookie.js
+++ utils/generateTokenAndSetCookie.js
@@ -4,12 +4,12 @@ export const generateTokenAndSetCookie = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
-  res.cookie("token", token, {
-    httpOnly: true,
-    sameSite: "strict",
-    secure: true,
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-  });
+  // res.cookie("token", token, {
+  //   httpOnly: true,
+  //   sameSite: "strict",
+  //   secure: true,
+  //   maxAge: 7 * 24 * 60 * 60 * 1000,
+  // });
 
   return token;
 };
